26c94fcde08e71a47b210fe460933d844cc71e13
John Lane <jlane75@gmail.com>
2020-11-28 10:17:30 -0500

    reactive contact from added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import 'hammerjs';
 //component imports:
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -58,7 +61,10 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [DishService, PromotionService, LeaderService], //drop your services in here
   bootstrap: [AppComponent], //this means to bootstrap the application we need to bootstrap the AppComponent, i.e. the root component
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
+
+  createForm(){
+    this.feedbackForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      telNum: 0,
+      email: '',
+      contactType: 'None',
+      message: ''
+
+    });
+  }
+
+  onSubmit(){
+    this.feedback=this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
+  } //value property of a form returns control values which in this case mirror structure of feedback class.
+    
+
 
 }
