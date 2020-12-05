0c17857d6b79f6c2aaf42cb5998bb19070b461f3
John Lane <jlane75@gmail.com>
2020-12-04 21:38:14 -0500

    god awful reactive programming with mindnumbing observables, part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,39 +11,93 @@ import { Feedback, ContactType } from '../shared/feedback';
 })
 export class ContactComponent implements OnInit {
 
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+
   @ViewChild('fform') feedbackFormDirective;
   
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
-  constructor(private fb: FormBuilder) { 
-    this.createForm();
-  }
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname':{
+      'required': 'First name is required',
+      'minLength': 'First name must be at least 2 characters long',
+      'maxLength': 'First name cannot be more than 25 charachters'
+    },
+    'lastname':{
+      'required': 'Last name is required',
+      'minLength': 'Last name must be at least 2 characters long',
+      'maxLength': 'Last name cannot be more than 25 charachters'
+    },
+    'telnum':{
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must only contain numbers.'
+    },
+    'email':{
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    }
+  };
 
   ngOnInit(): void {
     
   }
 
-  createForm(){
+  createForm(): void{
     this.feedbackForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      telNum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contactType: 'None',
       message: ''
-
     });
+
+    this.feedbackForm.valueChanges.
+      subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged(); //reset form validation messages
+  }
+
+  onValueChanged(data?: any){
+    if(!this.feedbackForm){
+      return;
+    }
+    const form = this.feedbackForm;
+    for(const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        //clear previous error messages if any
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid){
+          const messages = this.validationMessages[field];
+          for(const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit(){
     this.feedback=this.feedbackForm.value;//value property of a form returns control values which in this case mirror structure of feedback class.
     console.log(this.feedback);
     this.feedbackForm.reset({
-      firstName: '',
-      lastName: '',
-      telNum: '', 
+      firstname: '',
+      lastname: '',
+      telnum: '', 
       email: '', 
       agree: false,
       contacttype: 'None',
